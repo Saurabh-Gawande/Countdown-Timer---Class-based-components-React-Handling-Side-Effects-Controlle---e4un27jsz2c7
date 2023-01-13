3c0bd0b3caf4a6ce81cb4f93d5201c58dbad605b
Saurabh <saurabhgawande2016@gmail.com>
2023-01-13 19:09:32 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,41 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  
+    const [input, setInput] = useState("");
+    const [timeLeft, setTimeLeft] = useState(0);
+  
+    useEffect(() => {
+      let intervalId;
+      if (timeLeft > 0) {
+        intervalId = setInterval(() => {
+          setTimeLeft(timeLeft - 1);
+        }, 1000);
+      } else {
+        clearInterval(intervalId);
+      }
+      return () => clearInterval(intervalId);
+    }, [timeLeft]);
+
+    const handleKeyDown = event => {
+      if (event.keyCode === 13) {
+        const inputValue = parseInt(input);
+        if (!isNaN(inputValue) && inputValue > 0) {
+          setTimeLeft(inputValue);
+        } else {
+          setTimeLeft(0);
+        }
+      }
+    };
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleKeyDown} onChange={event => setInput(event.target.value)}/> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timeLeft}</div>
     </div>
   )
 }
